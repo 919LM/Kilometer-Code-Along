33fac746cdf2c8dde0d5e51c467dada1fbc69c8e
919LM <77459112+919LM@users.noreply.github.com>
2021-03-11 13:23:18 -0700

    Add files via upload

--- Kilometer Code-Along.py
+++ Kilometer Code-Along.py
@@ -5,23 +5,31 @@
     a. Add more values to a list
     b. Return a value to a specific index
 """
-
+myList = []
 def mainProgram(): 
-    myList = []
-    print("Hello, there! Let's work with lists!")
-    print("Choose from the following options. Type a number below!")
-    choice = input("1. Add to a list, 2. Return the value at an index position!     ")
-    if choice == "1": 
-        addToList()
-    elif choice == "2": 
-        indexValues()
-    #TO ADD: 1. A way to loop the action, 2. A way to quit
+    while True: 
+        print("Hello, there! Let's work with lists!")
+        print("Choose from the following options. Type a number below!")
+        choice = input("1. Add to a list, 2. Return the value at an index position! 3: Quit    ")
+        if choice == "1": 
+            addToList()
+        elif choice == "2": 
+            indexValues()
+        elif choice == "3":
+            break
+    #TO ADD: 1. A way to loop the action, 
+    # 2. A way to quit, 3. Exit Program
 def addToList(): 
     print("Adding to a list! Great choice!")
     newItem = input("Type an integer here!  ")
     myList.append(int(newItem))
 
 def indexValues(): 
+    print("Ohhh! I heard you need a particular piece of data!")
+    indexPos = input("What index position are you curious about?  ")
+    print (myList[int(indexPos)])
 
 if __name__ == "__main__": 
+
+
     mainProgram()
